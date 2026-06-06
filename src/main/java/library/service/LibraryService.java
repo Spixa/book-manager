26c77ced47e755b06a4be5b2a8e75345667593af
Spixa <spixa@github.com>
2026-06-06 13:00:44 +0330

    Utilize CSV file loading in service layer

--- src/main/java/library/service/LibraryService.java
+++ src/main/java/library/service/LibraryService.java
@@ -1,4 +1,21 @@
 package main.java.library.service;
 
+import main.java.library.model.Book;
+import main.java.library.util.FileManager;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryService {
+    List<Book> books;
+
+    public LibraryService() {
+        try {
+            books = FileManager.loadBooksFromCSV();
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+            books = new ArrayList<>();
+        }
+    }
 }
