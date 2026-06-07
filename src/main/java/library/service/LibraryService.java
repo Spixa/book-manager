c31f65bb8b24bcd3e831520a98aec7c10066f165
Spixa <spixa@github.com>
2026-06-07 15:53:09 +0330

    Add book list UI

--- src/main/java/library/service/LibraryService.java
+++ src/main/java/library/service/LibraryService.java
@@ -38,7 +38,7 @@ public class LibraryService {
         return false;
     }
 
-    private Optional<Book> findBookById(int id) {
+    public Optional<Book> findBookById(int id) {
         Optional<Book> bookOpt = Optional.empty();
         for (Book book : books) {
             if (book.getId() == id) {
@@ -48,5 +48,32 @@ public class LibraryService {
         }
 
         return bookOpt;
+    }
+
+    public List<Book> getAllBooks() {
+        return books;
+    }
+
+    public void saveBooks() throws IOException {
+        FileManager.saveBooksToCSV(books);
+    }
+
+    public boolean editBookMetadata(int bookId, String newTitle, String newAuthor, String newPublisher, Integer newYear) {
+        Optional<Book> opt = findBookById(bookId);
+        if (opt.isEmpty()) return false;
+
+        Book book = opt.get();
+        // shouldn't be empty NOR contain ONLY whitespace characters
+        if (newTitle != null && !newTitle.trim().isEmpty()) book.setTitle(newTitle.trim());
+        if (newAuthor != null && !newAuthor.trim().isEmpty()) book.setAuthor(newAuthor.trim());
+        if (newPublisher != null && !newPublisher.trim().isEmpty()) book.setPublisher(newPublisher.trim());
+        if (newYear != null) book.setPublicationYear(newYear);
+
+        try {
+            saveBooks();
+        } catch (IOException e) {
+            System.err.println("Ran into an error: " + e.getMessage());
+        }
+        return true;
     }
 }
